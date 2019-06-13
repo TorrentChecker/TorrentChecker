17998d0656fa6b09d2defebda9c0c9804c0b42a5
TorrentChecker <TorrentCheckerMail@gmail.com>
2019-06-13 20:49:04 +0300

    better timeout handling (removed HttpCompletionOption.ResponseHeadersRead)

--- TorrentChecker/My Project/AssemblyInfo.vb
+++ TorrentChecker/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)> 
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("609f8eb5-8cde-4d5a-9cf6-e8bba05c29b4")> 
+<Assembly: Guid("609f8eb5-8cde-4d5a-9cf6-e8bba05c29b4")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.0.0.0")> 
-<Assembly: AssemblyFileVersion("3.0.0.0")> 
+<Assembly: AssemblyVersion("3.0.2.0")>
+<Assembly: AssemblyFileVersion("3.0.2.0")>
