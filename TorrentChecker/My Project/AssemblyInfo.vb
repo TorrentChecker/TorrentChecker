1cd3da147bb0459199db5232bdd68bdc5de9b29b
TorrentChecker <TorrentCheckerMail@gmail.com>
2019-10-12 18:17:47 +0300

    version bump

--- TorrentChecker/My Project/AssemblyInfo.vb
+++ TorrentChecker/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.0.7.0")>
-<Assembly: AssemblyFileVersion("3.0.7.0")>
+<Assembly: AssemblyVersion("3.0.8.0")>
+<Assembly: AssemblyFileVersion("3.0.8.0")>
